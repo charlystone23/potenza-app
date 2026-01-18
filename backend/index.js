60cbc2c31db8996ac1fb9c0c116a9e8f3a5057b7
Charly Leal <carlitos007leal@gmail.com>
2026-01-18 13:50:52 -0300

    correccion

--- backend/index.js
+++ backend/index.js
@@ -262,6 +262,6 @@ app.post('/api/alumnos/:id/pagos', async (req, res) => {
 });
 
 // Start Server
-app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server running on port ${PORT}`);
 });
